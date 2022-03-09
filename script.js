5cdb66850969a8aabe0f77ff4a011f0b502ba893
EdmundsP <edmunds.platacs@gmail.com>
2022-03-09 22:53:38 +0200

    added about menu

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-/* .show klasi ar js jāpiešķi
-.drop_box nospiezot uz .btn_drop pogas */
-//1.
+
+//1.Dropdown menu ********************************
+let dropdown = document.querySelector('.dropdown')
+let drop_active_cont = document.querySelector('.drop_active_cont')
+let underline_shop = document.querySelector('.underline_shop')
 let btn_drop = document.querySelector('.btn_drop')
 let drop_box = document.querySelector('.drop_box')
+//let drop_content = document.querySelector('.drop_content')
 
-btn_drop.onmouseover = function(event){
+dropdown.onmouseover = function(event){
+    event.target.classList.contains('btn_drop')
     drop_box.style.display ='flex'
+    drop_active_cont.style.display ='flex' 
 }
-drop_box.onmouseleave = function(event){
+dropdown.onmouseleave = function(event){
+    event.target.classList.contains('btn_drop')
     drop_box.style.display ='none'
-    }
-
-
-
- // pazūd hover no btn_drop kad uzbraucu uz drop box
-//nepazūd menu ja no btn_drop nobrauc neuzbraucot drop_box
+    drop_active_cont.style.display ='none' 
+}
+drop_box.onmouseover = function(){
+    underline_shop.classList.remove('underline_shop')
+}
+drop_box.onmouseleave = function(){
+    underline_shop.classList.add('underline_shop')
+}
+// japieregulē drop_active_cont un btn_drop stilu 
 
 //2. open mobile menu and close mobile menu 
 let mobile_nav = document.querySelector('.small_screen_main')
